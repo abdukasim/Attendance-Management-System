861bf0e6a04663ca410ccc048242f77ac29365e0
abdukasim <abdukasimr@gmail.com>
2022-05-08 23:15:28 +0300

    attendance screen with list of people in attendance and a modal to log their attendance for the given day

---
+++ src/screens/AttendanceOfficer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import AttendanceScreen from "./AttendanceScreen";
+
+export default function AttendanceOfficer() {
+  return (
+    <View style={{ paddingBottom: 50 }}>
+      <AttendanceScreen />
+    </View>
+  );
+}
