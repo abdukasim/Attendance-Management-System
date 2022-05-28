595702915ddc38a50bc52196008f1bffa8877387
abdukasim <abdukasimr@gmail.com>
2022-05-28 22:25:43 +0300

    added next day button for testing purposes

--- src/screens/Attendance/AttendanceScreen.js
+++ src/screens/Attendance/AttendanceScreen.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 import CustomList from "../../components/CustomList";
 import { Overlay } from "react-native-elements";
 import url from "../../helpers/url";
 import AttendanceListModal from "../../components/AttendanceListModal";
 import { useFocusEffect } from "@react-navigation/native";
+import {
+  Button,
+  ButtonText,
+  MsgBox,
+  StyledButton,
+} from "../../components/styles";
 
 export default function AttendanceScreen() {
   const [inAttendanceList, setInAttendanceList] = useState([]);
   const [hasError, setHasError] = useState(false);
   const [AttendanceUser, setAttendanceUser] = useState({});
   const [visible, setVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [msgType, setMsgType] = useState("");
 
   const toggleOverlay = () => {
     setVisible(!visible);
@@ -37,8 +46,36 @@ export default function AttendanceScreen() {
     }, [])
   );
 
+  const nextDay = () => {
+    setIsLoading(true);
+    try {
+      const res = url.post("/api/attendance/client/attendance/invoke");
+      console.log(res);
+      setMsg("Successfully invoked next day attendance");
+      setMsgType("SUCCESS");
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setMsg("Error invoking next day attendance");
+      setMsgType("ERROR");
+      setIsLoading(false);
+    }
+  };
+
   return (
     <View>
+      {isLoading === false && (
+        <StyledButton onPress={() => nextDay()}>
+          <ButtonText>Next Day</ButtonText>
+        </StyledButton>
+      )}
+      {isLoading === true && (
+        <StyledButton onPress={() => nextDay()}>
+          <ActivityIndicator size="small" color="white" />
+        </StyledButton>
+      )}
+      <MsgBox type={msgType}>{msg}</MsgBox>
+
       <CustomList
         data={inAttendanceList}
         getUser={setAttendanceUser}
