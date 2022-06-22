81bb627c14c1c84d8ae9a1b3c75f03b5d18c6988
abdukasim <abdukasimr@gmail.com>
2022-06-22 14:46:12 +0300

    live update stats component

---
+++ src/components/Stats.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Stat, StatCard, StatTitle, StatValue } from "./styles";
+import { io } from "socket.io-client";
+
+export default function Stats() {
+  const [presentAttendees, setPresentAttendees] = useState(0);
+  const [servedMeals, setServedMeals] = useState(0);
+  const [beneficiaries, setBenefeciaries] = useState(0);
+
+  const socketRef = useRef();
+
+  useEffect(() => {
+    socketRef.current = io("http://muntaha.herokuapp.com/");
+    socketRef.current.on("present_count", (number) => {
+      console.log("present_count", number);
+      setPresentAttendees(number);
+    });
+    socketRef.current.on("meal_count", (number) => {
+      console.log("meal-count", number);
+      setServedMeals(number);
+    });
+    socketRef.current.on("client_count", (number) => {
+      console.log("client_count", number);
+      setBenefeciaries(number);
+    });
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    socketRef.current = io("http://muntaha.herokuapp.com/");
+    socketRef.current.on("present_count", (number) => {
+      console.log("present_count", number);
+      setPresentAttendees(number);
+    });
+    socketRef.current.on("meal_count", (number) => {
+      console.log("meal-count", number);
+      setServedMeals(number);
+    });
+    socketRef.current.on("client_count", (number) => {
+      console.log("client_count", number);
+      setBenefeciaries(number);
+    });
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, [presentAttendees, servedMeals, beneficiaries]);
+
+  return (
+    <StatCard>
+      <Stat>
+        <StatTitle>Present</StatTitle>
+        <StatValue>{presentAttendees}</StatValue>
+      </Stat>
+      <Stat>
+        <StatTitle>Served Meals</StatTitle>
+        <StatValue>{servedMeals}</StatValue>
+      </Stat>
+      <Stat>
+        <StatTitle>Total</StatTitle>
+        <StatValue>{beneficiaries}</StatValue>
+      </Stat>
+    </StatCard>
+  );
+}
