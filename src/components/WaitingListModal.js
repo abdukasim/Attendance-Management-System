0c3e99d964f5435a465d2d9ed99de4d0d8db15a0
abdukasim <abdukasimr@gmail.com>
2022-06-22 14:48:06 +0300

    major updates regarding image and audio upload, and formik fields

--- src/components/WaitingListModal.js
+++ src/components/WaitingListModal.js
@@ -1,5 +1,15 @@
-import React, { useState } from "react";
-import { View, ActivityIndicator, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  ActivityIndicator,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Image,
+  Button,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
 import {
   Colors,
   StyledModal,
@@ -12,24 +22,93 @@ import {
 } from "./styles";
 import CustomTextInput from "./CustomTextInput";
 import { Formik, FieldArray } from "formik";
-// import { Audio } from "expo-av";
 import RadioForm from "react-native-simple-radio-button";
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import url from "../helpers/url";
+import * as ImagePicker from "expo-image-picker";
+import axios from "axios";
+import { Audio } from "expo-av";
 
 const { brand, tertiary, darkLight, primary, secondary } = Colors;
 
 const WaitingListModal = ({ name, id, fetchWaitingList, toggleOverlay }) => {
-  // const [recording, setRecording] = useState();
-  // const [recordings, setRecordings] = useState([]);
   const [message, setMessage] = useState("");
   const [msgType, setMsgType] = useState("");
   const [show, setShow] = useState(false);
   const [date, setDate] = useState(new Date(2000, 0, 1));
   const [dob, setDob] = useState();
-  const [marital, setMaritalStatus] = useState();
+  const [marital, setMarital] = useState();
+  const [shelter, setShelter] = useState("");
   const [radioButton, setRadioButton] = useState();
+  const [image, setImage] = useState(null);
+
+  const [recording, setRecording] = useState();
+  const [audio, setSound] = useState();
+  const [playing, setPlaying] = useState(false);
+  const [audioUri, setAudioUri] = useState();
+
+  async function startRecording() {
+    try {
+      console.log("Requesting permissions..");
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      console.log("Starting recording..");
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI();
+    console.log("Recording stopped and stored at", uri);
+    setAudioUri(uri);
+    console.log(typeof audioUri);
+    // FormikProps.setFieldValue("audio", uri);
+  }
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync({ uri: audioUri });
+    setSound(sound);
+    console.log("Souncd loaded", audio);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+    sound.setOnPlaybackStatusUpdate(async (status) => {
+      console.log("Status", status);
+      status.isPlaying && setPlaying(true);
+      if (status.didJustFinish) {
+        console.log("Finished");
+        setPlaying(false);
+        await sound.unloadAsync();
+      }
+    });
+  }
+
+  async function pauseSound() {
+    console.log("Pausing Sound");
+    await audio.pauseAsync();
+    setPlaying(false);
+  }
+  // React.useEffect(() => {
+  //   return audio
+  //     ? () => {
+  //         console.log("Unloading Sound");
+  //         audio.unloadAsync();
+  //       }
+  //     : undefined;
+  // }, [audio]);
 
   const radio_props = [
     { label: "Yes", value: 0 },
@@ -47,27 +126,72 @@ const WaitingListModal = ({ name, id, fetchWaitingList, toggleOverlay }) => {
     setShow(true);
   };
 
+  function getFormData(object) {
+    const formData = new FormData();
+    formData.append("image", {
+      name: new Date() + "_profile.jpg",
+      uri: object.image,
+      type: "image/jpg",
+    });
+    formData.append("recording_1", {
+      name: new Date() + "_audio.m4a",
+      uri: audioUri,
+      type: "audio/m4a",
+    });
+    formData.append("birthday", dob.toDateString());
+    Object.keys(object).forEach((key) => {
+      if (key !== "image" && key !== "recording" && key !== "birthday") {
+        formData.append(key, object[key]);
+      }
+    });
+    console.log(formData);
+    return formData;
+  }
   const handleWaitingListForm = (data, setSubmitting) => {
-    url
-      .post("/api/attendance/registration/visit", data)
+    console.log("data: ", data);
+    let bodyData = getFormData(data);
+
+    fetch("https://muntaha.herokuapp.com/api/attendance/registration/visit", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "multipart/form-data",
+      },
+      body: bodyData,
+    })
       .then((res) => {
-        res.status === 200 && setSubmitting(false);
         console.log(res.status);
         setMsgType("SUCCESS");
-        setMessage("Successfully added to waiting list");
+        setMessage("Successfully added to visited list");
         setTimeout(() => {
           toggleOverlay();
           fetchWaitingList();
         }, 1000);
       })
       .catch((err) => {
-        console.log(err.response.data.description);
-        setMsgType("ERROR");
-        setMessage("Failed to update user!");
+        if (
+          err.message === "Network Error" ||
+          err.message === "Network request failed"
+        ) {
+          setMsgType("SUCCESS");
+          setMessage("Successfully added to visited list");
+          setTimeout(() => {
+            toggleOverlay();
+            fetchWaitingList();
+          }, 1000);
+        } else {
+          setMsgType("ERROR");
+          setMessage(err.message);
+        }
+      })
+      .finally(() => {
         setSubmitting(false);
       });
   };
 
+  useEffect(() => {
+    console.log("image: ", image);
+  }, [image]);
   return (
     <StyledModal>
       {show && (
@@ -93,8 +217,22 @@ const WaitingListModal = ({ name, id, fetchWaitingList, toggleOverlay }) => {
           remark: "",
         }}
         onSubmit={(values, { setSubmitting }) => {
-          console.log(values);
-          handleWaitingListForm(values, setSubmitting);
+          // check if values are empty
+          if (
+            values.maritalStatus === "" ||
+            values.jobStatus === "" ||
+            values.rent === ""
+          ) {
+            setMsgType("ERROR");
+            setMessage("Please fill all the fields");
+            setTimeout(() => {
+              setMsgType("");
+              setMessage("");
+            }, 2000);
+            setSubmitting(false);
+          } else {
+            handleWaitingListForm(values, setSubmitting);
+          }
         }}
       >
         {({
@@ -105,34 +243,129 @@ const WaitingListModal = ({ name, id, fetchWaitingList, toggleOverlay }) => {
           setFieldValue,
         }) => (
           <StyledFormArea>
+            <View style={imageUploaderStyles.container}>
+              {image && (
+                <Image
+                  source={{ uri: image }}
+                  style={{ width: 150, height: 150 }}
+                />
+              )}
+              <View style={imageUploaderStyles.uploadBtnContainer}>
+                <TouchableOpacity
+                  onPress={async () => {
+                    let _image = await ImagePicker.launchImageLibraryAsync({
+                      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                      allowsEditing: true,
+                      aspect: [4, 4],
+                      quality: 0.5,
+                    });
+
+                    console.log(JSON.stringify(_image));
+
+                    if (!_image.cancelled) {
+                      setImage(_image.uri);
+                      setFieldValue("image", _image.uri);
+                    }
+                  }}
+                  style={imageUploaderStyles.uploadBtn}
+                >
+                  <Text>{image ? "Edit" : "Upload"} Image</Text>
+                  <FontAwesome name="camera" size={20} color="black" />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <StyledInputLabel>Record Audio</StyledInputLabel>
+            <View style={styles.centeredView}>
+              <TouchableOpacity
+                onPress={recording ? stopRecording : startRecording}
+                style={{
+                  ...styles.buttonStyle,
+                  backgroundColor: recording ? "red" : "green",
+                }}
+              >
+                <Text style={{ color: "white" }}>
+                  {recording ? (
+                    <FontAwesome name="stop" size={20} color="white" />
+                  ) : (
+                    <FontAwesome name="microphone" size={20} color="white" />
+                  )}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={playing ? pauseSound : playSound}
+                style={{ ...styles.buttonStyle, backgroundColor: brand }}
+              >
+                {playing ? (
+                  <FontAwesome name="pause" size={20} color="white" />
+                ) : (
+                  <FontAwesome name="play" size={20} color="white" />
+                )}
+              </TouchableOpacity>
+            </View>
+
             <CustomTextInput
-              label="Birthday"
+              label="Age"
               name="birthday"
               icon="calendar"
-              placeholder="birthday"
+              placeholder="age"
               placeholderTextColor={darkLight}
               onChangeText={handleChange("birthday")}
+              value={values.birthday}
+              keyboardType="numeric"
               // onBlur={handleBlur}
-              value={dob ? dob.toDateString() : ""}
-              isDate={true}
-              editable={false}
-              showDatePicker={showDatePicker}
             />
+
+            <StyledInputLabel>Shelter Status</StyledInputLabel>
+            <Picker
+              prompt={"Select"}
+              selectedValue={values.rent}
+              onValueChange={(itemValue, itemIndex) => {
+                if (itemValue !== "default") {
+                  setFieldValue("rent", itemValue);
+                  setShelter(itemValue);
+                }
+              }}
+              style={styles.pickerStyle}
+            >
+              <Picker.Item
+                label="Please select shelter status"
+                value="default"
+              />
+              <Picker.Item label="Rent" value="Rent" />
+              <Picker.Item label="Private" value="Private" />
+              <Picker.Item label="Dependent" value="Dependent" />
+              <Picker.Item label="Homeless" value="Homeless" />
+            </Picker>
+
+            {shelter === "rent" && (
+              <CustomTextInput
+                label="Rent Amount"
+                name="rentAmount"
+                icon="money"
+                placeholder="rent amount"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange("rentAmount")}
+                value={values.rentAmount}
+              />
+            )}
 
             <StyledInputLabel>Marital Status</StyledInputLabel>
             <Picker
               prompt={"Select Marital"}
               selectedValue={values.maritalStatus}
               onValueChange={(itemValue, itemIndex) => {
-                // setSelectedSex(itemValue);
-                setMaritalStatus(itemValue);
-                console.log(marital);
-                setFieldValue("maritalStatus", itemValue);
-                // console.log(itemValue);
+                if (itemValue !== "default") {
+                  setMarital(itemValue);
+                  setFieldValue("maritalStatus", itemValue);
+                }
               }}
               style={styles.pickerStyle}
             >
-              <Picker.Item label="" value="" />
+              <Picker.Item
+                label="Please select marital status"
+                value="default"
+              />
               <Picker.Item label="Divorced" value="Divorced" />
               <Picker.Item label="Married" value="Married" />
               <Picker.Item label="Widowed" value="Widowed" />
@@ -176,42 +409,66 @@ const WaitingListModal = ({ name, id, fetchWaitingList, toggleOverlay }) => {
                         <View key={index}>
                           <CustomTextInput
                             label="Child Name"
-                            name={`children.${index}.name`}
+                            name={`children[${index}].name`}
                             icon="child"
-                            placeholder="children"
+                            placeholder="Name"
                             placeholderTextColor={darkLight}
-                            // onChangeText={handleChange("children")}
+                            onChangeText={handleChange(
+                              `children[${index}].name`
+                            )}
+                            value={child.name}
                             // onBlur={handleBlur}
                           />
                           <CustomTextInput
                             label="Child Age"
-                            name={`children.${index}.age`}
+                            name={`children[${index}].age`}
                             icon="child"
-                            placeholder="children"
+                            placeholder="Age"
                             placeholderTextColor={darkLight}
-                            // onChangeText={handleChange("children")}
+                            onChangeText={handleChange(
+                              `children[${index}].age`
+                            )}
+                            value={child.age}
+                            keyboardType="numeric"
                             // onBlur={handleBlur}
                           />
                           <CustomTextInput
                             label="Child Schooling"
-                            name={`children.${index}.schooling`}
+                            name={`children[${index}].schooling`}
                             icon="child"
-                            placeholder="children"
+                            placeholder="Schooling"
                             placeholderTextColor={darkLight}
-                            // onChangeText={handleChange("children")}
+                            onChangeText={handleChange(
+                              `children[${index}].schooling`
+                            )}
+                            value={child.schooling}
                             // onBlur={handleBlur}
                           />
                           <View style={styles.childrenButton}>
-                            <StyledButton onPress={() => remove(index)}>
-                              <ButtonText>Remove</ButtonText>
-                            </StyledButton>
-                            <StyledButton
+                            <TouchableOpacity
+                              onPress={() => remove(index)}
+                              style={{
+                                ...styles.buttonStyle,
+                                backgroundColor: "red",
+                              }}
+                            >
+                              <Text style={{ color: "white", fontSize: 20 }}>
+                                -
+                              </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
                               onPress={() =>
                                 push({ name: "", age: "", schooling: "" })
                               }
+                              style={{
+                                ...styles.buttonStyle,
+                                backgroundColor: "green",
+                              }}
                             >
-                              <ButtonText>Add Child</ButtonText>
-                            </StyledButton>
+                              <Text style={{ color: "white", fontSize: 20 }}>
+                                +
+                              </Text>
+                            </TouchableOpacity>
                           </View>
                         </View>
                       ))}
@@ -232,18 +489,6 @@ const WaitingListModal = ({ name, id, fetchWaitingList, toggleOverlay }) => {
             />
 
             <CustomTextInput
-              label="Rent"
-              name="rent"
-              icon="money"
-              placeholder="rent"
-              placeholderTextColor={darkLight}
-              onChangeText={handleChange("rent")}
-              // onBlur={handleBlur}
-              value={values.rent}
-              keyboardType="numeric"
-            />
-
-            <CustomTextInput
               label="Health"
               name="health"
               icon="home"
@@ -253,16 +498,7 @@ const WaitingListModal = ({ name, id, fetchWaitingList, toggleOverlay }) => {
               // onBlur={handleBlur}
               value={values.health}
             />
-            <CustomTextInput
-              label="remark"
-              name="remark"
-              icon="home"
-              placeholder="remark"
-              placeholderTextColor={darkLight}
-              onChangeText={handleChange("remark")}
-              // onBlur={handleBlur}
-              value={values.remark}
-            />
+
             <MsgBox type={msgType}>{message}</MsgBox>
             {!isSubmitting && (
               <StyledButton onPress={handleSubmit}>
@@ -284,9 +520,10 @@ const WaitingListModal = ({ name, id, fetchWaitingList, toggleOverlay }) => {
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: "center",
+    flexDirection: "row",
+    justifyContent: "flex-start",
     alignItems: "center",
-    marginTop: 22,
+    marginBottom: 22,
   },
   modalView: {
     margin: 20,
@@ -355,8 +592,43 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 16,
   },
-  button: {
-    margin: 16,
+  buttonStyle: {
+    padding: 10,
+    width: 50,
+    height: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 999,
+    marginRight: 20,
+  },
+});
+
+const imageUploaderStyles = StyleSheet.create({
+  container: {
+    elevation: 2,
+    height: 150,
+    width: 150,
+    backgroundColor: "#efefef",
+    position: "relative",
+    borderRadius: 999,
+    overflow: "hidden",
+    marginBottom: 20,
+  },
+  uploadBtnContainer: {
+    opacity: 0.7,
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    backgroundColor: "lightgrey",
+    width: "100%",
+    height: "40%",
+  },
+  uploadBtn: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
   },
 });
 
