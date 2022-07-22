f839df526d63a1bd42d19cc967f898b554ff8181
Abdulrahman Kasim <abdukasimr@gmail.com>
2022-07-22 21:39:06 +0300

    removed forwardRef and added innerRef as a prop

--- src/components/CustomTextInput.js
+++ src/components/CustomTextInput.js
@@ -1,5 +1,5 @@
-import React, { forwardRef } from "react";
-import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import {
   Colors,
@@ -11,52 +11,48 @@ import {
 
 const { brand, darkLight } = Colors;
 
-const CustomTextInput = forwardRef(
-  (
-    {
-      label,
-      icon,
-      isPassword,
-      hidePassword,
-      setHidePassword,
-      field: { name, onBlur, onChange, value },
-      form: { errors, touched, setFieldTouched },
-      ...props
-    },
-    ref
-  ) => {
-    const hasError = errors[name] && touched[name];
-    return (
-      <View>
-        <LeftIcon>
-          <FontAwesome name={icon} size={30} color={brand} />
-        </LeftIcon>
-        <StyledInputLabel>{label}</StyledInputLabel>
-        <StyledTextInput
-          value={value}
-          onChangeText={(text) => onChange(name)(text)}
-          onBlur={() => {
-            setFieldTouched(name);
-            onBlur(name);
-          }}
-          {...props}
-          style={hasError && styles.errorInput}
-          ref={ref}
-        />
-        {hasError && <Text style={styles.errorText}>{errors[name]}</Text>}
-        {isPassword && (
-          <RightIcon onPress={() => setHidePassword(!hidePassword)}>
-            <FontAwesome
-              size={30}
-              color={darkLight}
-              name={hidePassword ? "eye" : "eye-slash"}
-            />
-          </RightIcon>
-        )}
-      </View>
-    );
-  }
-);
+const CustomTextInput = ({
+  label,
+  icon,
+  isPassword,
+  hidePassword,
+  setHidePassword,
+  field: { name, onBlur, onChange, value },
+  form: { errors, touched, setFieldTouched },
+  innerRef,
+  ...props
+}) => {
+  const hasError = errors[name] && touched[name];
+  return (
+    <View>
+      <LeftIcon>
+        <FontAwesome name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput
+        value={value}
+        onChangeText={(text) => onChange(name)(text)}
+        onBlur={() => {
+          setFieldTouched(name);
+          onBlur(name);
+        }}
+        {...props}
+        style={hasError && styles.errorInput}
+        ref={innerRef}
+      />
+      {hasError && <Text style={styles.errorText}>{errors[name]}</Text>}
+      {isPassword && (
+        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+          <FontAwesome
+            size={30}
+            color={darkLight}
+            name={hidePassword ? "eye" : "eye-slash"}
+          />
+        </RightIcon>
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   errorText: {
